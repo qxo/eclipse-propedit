b1786e0e5aa8f65da64ba28f1a44cb40f5eddeca
chomakichi <chomakichi@74e17e67-1e22-0410-8afe-952abe9dccea>
2009-05-05 15:01:36 +0000

    HyperlinkDetectorを追加できる拡張ポイントを作成

--- PropertiesEditor/src/jp/gr/java_conf/ussiy/app/propedit/eclipse/plugin/editors/PropertiesConfiguration.java
+++ PropertiesEditor/src/jp/gr/java_conf/ussiy/app/propedit/eclipse/plugin/editors/PropertiesConfiguration.java
@@ -1,13 +1,26 @@
 package jp.gr.java_conf.ussiy.app.propedit.eclipse.plugin.editors;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import jp.gr.java_conf.ussiy.app.propedit.eclipse.plugin.PropertiesEditorPlugin;
 import jp.gr.java_conf.ussiy.app.propedit.eclipse.plugin.preference.PropertiesEditorPreference;
 
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.IConfigurationElement;
+import org.eclipse.core.runtime.IExtension;
+import org.eclipse.core.runtime.IExtensionPoint;
+import org.eclipse.core.runtime.IExtensionRegistry;
+import org.eclipse.core.runtime.ILog;
+import org.eclipse.core.runtime.IStatus;
+import org.eclipse.core.runtime.Platform;
+import org.eclipse.core.runtime.Status;
 import org.eclipse.jface.preference.IPreferenceStore;
 import org.eclipse.jface.preference.PreferenceConverter;
 import org.eclipse.jface.text.IDocument;
 import org.eclipse.jface.text.ITextDoubleClickStrategy;
 import org.eclipse.jface.text.TextAttribute;
+import org.eclipse.jface.text.hyperlink.IHyperlinkDetector;
 import org.eclipse.jface.text.presentation.IPresentationReconciler;
 import org.eclipse.jface.text.presentation.PresentationReconciler;
 import org.eclipse.jface.text.reconciler.IReconciler;
@@ -19,6 +32,8 @@ import org.eclipse.swt.graphics.Color;
 import org.eclipse.swt.graphics.RGB;
 
 public class PropertiesConfiguration extends SourceViewerConfiguration {
+	
+	private static final String EXTENSION_POINT = "jp.gr.java_conf.ussiy.app.propedit.hyperlinkdetectors"; //$NON-NLS-1$
 
 	private PropertiesDoubleClickStrategy doubleClickStrategy;
 
@@ -95,6 +110,45 @@ public class PropertiesConfiguration extends SourceViewerConfiguration {
         MonoReconciler reconciler = new MonoReconciler(strategy,false);
         
         return reconciler;
+	}
+
+	/**
+	 * @see org.eclipse.jface.text.source.SourceViewerConfiguration#getHyperlinkDetectors(org.eclipse.jface.text.source.ISourceViewer)
+	 */
+	public IHyperlinkDetector[] getHyperlinkDetectors(ISourceViewer viewer) {
+		IHyperlinkDetector[] detectors = super.getHyperlinkDetectors(viewer);
+		List list = new ArrayList();
+		for (int i = 0; i < detectors.length; i++) {
+			if (detectors[i] != null) list.add(detectors[i]);
+		}
+		list.addAll(computePropertiesHyperlinkDetectors());
+		detectors = (IHyperlinkDetector[])list.toArray(new IHyperlinkDetector[0]);
+		return detectors;
+	}
+
+	protected List computePropertiesHyperlinkDetectors() {
+		IExtensionRegistry registry = Platform.getExtensionRegistry();
+		IExtensionPoint extensionPoint = registry.getExtensionPoint(EXTENSION_POINT);
+		IExtension[] extensions = extensionPoint.getExtensions();
+		ArrayList results = new ArrayList();
+		for (int i = 0; i < extensions.length; i++) {
+			IConfigurationElement[] elements = extensions[i].getConfigurationElements();
+			for (int j = 0; j < elements.length; j++) {
+				try {
+					Object detector = elements[j].createExecutableExtension("class"); //$NON-NLS-1$
+					if (detector instanceof jp.gr.java_conf.ussiy.app.propedit.eclipse.plugin.editors.detector.IHyperlinkDetector) {
+						((jp.gr.java_conf.ussiy.app.propedit.eclipse.plugin.editors.detector.IHyperlinkDetector)detector).setTextEditor(editor);
+						results.add(detector);
+					}
+				} catch(CoreException e) {
+					IStatus status = new Status(IStatus.ERROR, PropertiesEditorPlugin.PLUGIN_ID, 0, e.getMessage(), e);
+					ILog log = PropertiesEditorPlugin.getDefault().getLog();
+					log.log(status);
+				}
+			}
+		}
+		
+		return results;
 	}
 	
 }
