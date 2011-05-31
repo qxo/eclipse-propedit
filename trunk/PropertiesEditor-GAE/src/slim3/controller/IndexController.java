896646955924f186ba38256d10907affbfeb1a24
chomakichi <chomakichi@74e17e67-1e22-0410-8afe-952abe9dccea>
2011-05-31 13:28:57 +0000

    Share project "PropertiesEditor-GAE" into "https://svn.sourceforge.jp/svnroot/propedit"

---
+++ trunk/PropertiesEditor-GAE/src/slim3/controller/IndexController.java
@@ -0,0 +1,123 @@
+package slim3.controller;
+
+import java.net.URLDecoder;
+import java.net.URLEncoder;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.servlet.http.Cookie;
+
+import org.slim3.controller.Controller;
+import org.slim3.controller.Navigation;
+
+import slim3.lib.EncodeChanger;
+
+public class IndexController extends Controller {
+
+    @Override
+    public Navigation run() throws Exception {
+        String src1 = asString("src1");
+        String src2 = asString("src2");
+
+        String cvt1 = EncodeChanger.unicode2UnicodeEsc(src1);
+        String cvt2 = EncodeChanger.unicodeEsc2Unicode(src2);
+
+        requestScope("src1", src1);
+        requestScope("cvt1", cvt1);
+        requestScope("src2", src2);
+        requestScope("cvt2", cvt2);
+        
+        String cookieValue1 = "";
+        String cookieValue2 = "";
+        if (src1 != null && !src1.equals("")) {
+            cookieValue1 = URLEncoder.encode(src1, "UTF-8");
+        }
+        if (src2 != null && !src2.equals("")) {
+            cookieValue2 = URLEncoder.encode(src2, "UTF-8");
+        }
+        List<String> options1 = new ArrayList<String>();
+        List<String> options2 = new ArrayList<String>();
+        Cookie[] cookies = request.getCookies();
+        if (cookies != null) {
+            for (Cookie cookie : cookies) {
+                if (cookie.getName().equals("statement1")) {
+                    String cookieValue = cookie.getValue();
+                    String[] items = cookieValue.split("<>");
+                    for (String item : items) {
+                        options1.remove(item);
+                        options1.add(item);
+                        if (options1.size() > 20) {
+                            options1.remove(0);
+                        }
+                    }
+                } else if (cookie.getName().equals("statement2")) {
+                    String cookieValue = cookie.getValue();
+                    String[] items = cookieValue.split("<>");
+                    for (String item : items) {
+                        options2.remove(item);
+                        options2.add(item);
+                        if (options2.size() > 20) {
+                            options2.remove(0);
+                        }
+                    }
+                }
+            }
+        }
+        if (cookieValue1 != null && !cookieValue1.equals("")) {
+            options1.remove(cookieValue1);
+            options1.add(cookieValue1);
+            if (options1.size() > 20) {
+                options1.remove(0);
+            }
+        }
+        if (cookieValue2 != null && !cookieValue2.equals("")) {
+            options2.remove(cookieValue2);
+            options2.add(cookieValue2);
+            if (options2.size() > 20) {
+                options2.remove(0);
+            }
+        }
+        StringBuffer cookieBuffer1 = new StringBuffer();
+        for (String option : options1) {
+            if (cookieBuffer1.length() == 0) {
+                cookieBuffer1.append(option);
+            } else {
+                cookieBuffer1.append("<>").append(option);
+            }
+        }
+        StringBuffer cookieBuffer2 = new StringBuffer();
+        for (String option : options2) {
+            if (cookieBuffer2.length() == 0) {
+                cookieBuffer2.append(option);
+            } else {
+                cookieBuffer2.append("<>").append(option);
+            }
+        }
+        Cookie cookie1 = new Cookie("statement1", cookieBuffer1.toString());
+        cookie1.setMaxAge(60 * 60 * 24 * 365);
+        response.addCookie(cookie1);
+        Cookie cookie2 = new Cookie("statement2", cookieBuffer2.toString());
+        cookie2.setMaxAge(60 * 60 * 24 * 365);
+        response.addCookie(cookie2);
+        
+        List<String> op1 = new ArrayList<String>();
+        for (int i = options1.size() - 1; i >= 0; i--) {
+            if (isBlankOrNull(options1.get(i))) continue;
+            op1.add(URLDecoder.decode(options1.get(i), "UTF-8"));
+        }
+        List<String> op2 = new ArrayList<String>();
+        for (int i = options2.size() - 1; i >= 0; i--) {
+            if (isBlankOrNull(options2.get(i))) continue;
+            op2.add(URLDecoder.decode(options2.get(i), "UTF-8"));
+        }
+        request.setAttribute("options1", op1);
+        request.setAttribute("options2", op2);
+
+        return forward("index.jsp");
+    }
+
+    public static boolean isBlankOrNull(String str) {
+        return (str == null || str.equals(""));
+    }
+}
+
---
+++ trunk/PropertiesEditor-GAE/war/index.jsp
@@ -0,0 +1,110 @@
+<%@page pageEncoding="UTF-8" isELIgnored="false" session="false"%>
+<%@taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core"%>
+<%@taglib prefix="fn" uri="http://java.sun.com/jsp/jstl/functions"%>
+<%@taglib prefix="f" uri="http://www.slim3.org/functions"%>
+
+<html>
+  <head>
+    <meta http-equiv="content-type" content="text/html; charset=UTF-8">
+	<META name="keywords" content="java,native2ascii,eclipse,multibyte,plugin,multibyte,property,editor,Code,conversion,application,unicode,properties,editor,setting,file,hover,コード変換,アプリケーション,ユニコード,プロパティ,エディタ,設定,ファイル,ホバー">
+    <title>Unicode reference character changer</title>
+    <script type="text/javascript">
+	<!--
+	function historyChange(textareaid, selectobject) {
+		var t = document.getElementById(textareaid);
+		t.value = selectobject.value;
+	}
+	// -->
+    </script>
+    <style type="text/css">
+    .title {
+    	font-size: 1.3em;
+    	font-weight: bold;
+    }
+    th {
+    	background-color: #55F;
+    	color: #FFF;
+    	text-align: center;
+    	padding: 3px;
+    }
+    </style>
+  </head>
+
+  <body>
+  	<p class="title">Unicode converter</p>
+    <form name="form1" action="index" method="post">
+    <table style="width: 100%">
+      <tr>
+        <td colspan="2">convert to Unicode reference characters. <input type="submit" value="convert"/></td>
+      </tr>
+      <tr>
+      	<th>plain text</th><th>text converted into Unicode reference characters</th>
+      </tr>
+      <tr>
+      	<td style="width: 50%"><textarea id="src1" name="src1" style="width: 100%" rows="5">${f:h(src1)}</textarea></td>
+      	<td style="width: 50%"><textarea name="cvt1" style="width: 100%" rows="5" readonly="readonly">${f:h(cvt1)}</textarea></td>
+      </tr>
+      <tr>
+      	<td>
+      		<select onchange="historyChange('src1', this)" style="width: 300px;">
+      			<option value=""></option>
+      			<c:forEach var="option" items="${options1}">
+      			<option value="${f:h(option)}">${f:h(option)}</option>
+      			</c:forEach>
+      		</select>
+      	</td>
+      	<td></td>
+      </tr>
+      <tr>
+        <td colspan="2">unescape Unicode reference characters. <input type="submit" value="convert"/></td>
+      </tr>
+      <tr>
+      	<th>text converted into Unicode reference characters</th><th>plain text</th>
+      </tr>
+      <tr>
+      	<% if (request.getAttribute("src2") == null) request.setAttribute("src2", ""); %>
+        <td style="width: 50%"><textarea id="src2" name="src2" style="width: 100%" rows="5"><%= request.getAttribute("src2") %></textarea></td>
+      	<% if (request.getAttribute("cvt2") == null) request.setAttribute("cvt2", ""); %>
+        <td style="width: 50%"><textarea name="cvt2" style="width: 100%" rows="5" readonly="readonly"><%= request.getAttribute("cvt2") %></textarea></td>
+      </tr>
+      <tr>
+      	<td>
+      		<select onchange="historyChange('src2', this)" style="width: 300px;">
+      			<option value=""></option>
+      			<c:forEach var="option" items="${options2}">
+      			<option value="${f:h(option)}">${f:h(option)}</option>
+      			</c:forEach>
+      		</select>
+      	</td>
+      	<td></td>
+      </tr>
+    </table>
+    </form>
+<script type="text/javascript">
+
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-10552398-4']);
+  _gaq.push(['_trackPageview']);
+
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
+
+</script>
+<!-- Start of Woopra Code -->
+<script type="text/javascript">
+var woo_settings = {idle_timeout:'60000', domain:'propedit.appspot.com'};
+(function(){
+var wsc = document.createElement('script');
+wsc.src = document.location.protocol+'//static.woopra.com/js/woopra.js';
+wsc.type = 'text/javascript';
+wsc.async = true;
+var ssc = document.getElementsByTagName('script')[0];
+ssc.parentNode.insertBefore(wsc, ssc);
+})();
+</script>
+<!-- End of Woopra Code -->
+  </body>
+</html>
